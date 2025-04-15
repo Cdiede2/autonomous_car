aa4c85a9d9f2946e4e893f6350ca313a038865f7
cdiede2 <cdiede2@uic.edu>
2025-04-15 18:31:34 -0500

    Stable SI Pulse. Working on ADC atten/ref voltage lvl

--- src/setup.c
+++ src/setup.c
@@ -1,9 +1,15 @@
 #include "setup.h"
 
+// Timer Configuration
+#define TIMER_PERIOD_LF 4 // Timer period in milliseconds
+#define TIMER_PERIOD_HF 2 // Timer period in milliseconds
+
+
 void setup() {
     const char *TAG = "SETUP";
     ESP_LOGI(TAG, "Setting up...");
 
+    //////////////////
     // Setup GPIO
     unsigned long long gpio_mask = 0ULL;
     gpio_mask |= (1ULL << CLK_OUTP_PIN);  // GPIO2
@@ -18,18 +24,39 @@ void setup() {
     };
 
     gpio_config(&io_conf); // Configure GPIO
+    //////////////////
 
-    // Setup Timer
-    esp_timer_handle_t timer_handle;
-    const esp_timer_create_args_t timer_args = {
-        .callback = &timer_isr,                 // Timer ISR callback
-        .name = "timer"                         // Timer name
+
+    //////////////////
+    // Setup Low Frequency Timer
+    esp_timer_handle_t lf_timer_handle;
+
+
+    const esp_timer_create_args_t lf_timer_args = {
+        .callback   = &lf_timer_isr,                // Timer ISR callback
+        .name       = "timer"                       // Timer name
     };
 
-    adc1_config_width(ADC_WIDTH_BIT_12); // Set ADC width to 12 bits
-    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11); // Set attenuation
 
-    esp_timer_create(&timer_args, &timer_handle);   // Create timer
-    esp_timer_start_periodic(timer_handle, TIMER_PERIOD); 
+    esp_timer_create(&lf_timer_args, &lf_timer_handle);   // Create timer
+    esp_timer_start_periodic(lf_timer_handle, TIMER_PERIOD_LF * 1000); 
+    //////////////////
+    
+
+    //////////////////
+    // Setup High Frequency Timer
+    esp_timer_handle_t hf_timer_handle;
+
+    const esp_timer_create_args_t hf_timer_args = {
+        .callback   = &hf_timer_isr,                // Timer ISR callback
+        .name       = "timer"                       // Timer name
+    };
+
+    esp_timer_create(&hf_timer_args, &hf_timer_handle);
+    esp_timer_start_periodic(hf_timer_handle, TIMER_PERIOD_HF * 1000);
+    //////////////////
+    
+    adc1_config_width(ADC_WIDTH_BIT_12); // Set ADC width to 12 bits
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_0); // Set attenuation
     return;
 }
